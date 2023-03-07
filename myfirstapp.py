6c743da9675ad2579444ef5b3874f2913f9fba62
etiqahcanalytics <126073595+etiqahcanalytics@users.noreply.github.com>
2023-03-07 12:05:57 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -30,3 +30,17 @@ elif option=='T n C':
         'Intplan': ['yes', 'yes', 'yes', 'no'],
         'Churn Status': [0, 0, 0, 1]
         }))
+
+else:
+    'Starting a long computation...'
+    
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+   
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+    '...and now we\'re done!'
