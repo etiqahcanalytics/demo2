fb044c08e9dbb06c6ccbac872bc0ac1008898a9c
etiqahcanalytics <126073595+etiqahcanalytics@users.noreply.github.com>
2023-03-07 12:23:33 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -38,10 +38,10 @@ else:
     latest_iteration = st.empty()
     bar = st.progress(0)
 
-    for i in range(50):
+    for i in range(1000):
    
         latest_iteration.text(f'Iteration {i+1}')
         bar.progress(i + 1)
-        time.sleep(0.1)
+        time.sleep(1)
 
     '...and now we\'re done!'
